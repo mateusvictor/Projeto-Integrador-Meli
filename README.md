d43702ab5d1afa14c20f55b15df99014f8ed870f
mateusvictor <mateus.ssilva@mercadolivre.com>
2022-04-22 15:54:58 -0300

    add project structure

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Projeto-Integrador-Meli
+
 Repositório contendo o Projeto Integrador do Bootcamp IT - Mercado Livre
---
+++ src/test/java/br/com/meli/fresh/FreshApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.fresh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
