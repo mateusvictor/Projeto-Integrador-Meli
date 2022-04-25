245c6292186f7391c0ba9991cdaad442164db585
Luiz Felipe R. Silveira <luiz.silveira@mercadolivre.com>
2022-04-25 16:21:44 -0300

    WIP Finished both GET functions (getByID and getAll) implementations

--- src/main/java/br/com/meli/fresh/controller/ProductController.java
+++ src/main/java/br/com/meli/fresh/controller/ProductController.java
@@ -2,18 +2,21 @@ package br.com.meli.fresh.controller;
 
 import br.com.meli.fresh.assembler.ProductMapper;
 import br.com.meli.fresh.dto.request.ProductRequest;
+import br.com.meli.fresh.dto.response.productResponse.ProductResponse;
 import br.com.meli.fresh.model.Product;
 import br.com.meli.fresh.services.impl.ProductServiceImpl;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.util.stream.Collectors;
+
 @RestController
-@RequestMapping("api/v1/fresh-products/product")
+@RequestMapping("api/v1/fresh-products/products")
 @AllArgsConstructor
 public class ProductController {
 
@@ -30,5 +33,21 @@ public class ProductController {
                 .toUri())
                 .build();
 
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<ProductResponse> getProductById(@PathVariable String id){
+        return ResponseEntity.ok(
+                mapper.toResponseObject(service.getById(id))
+        );
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ProductResponse>> getAllProducts(Pageable pageable){
+        return ResponseEntity.ok(
+                new PageImpl<ProductResponse>(
+                        service.getAll(pageable).stream().map(mapper::toResponseObject).collect(Collectors.toList())
+                )
+        );
     }
 }
