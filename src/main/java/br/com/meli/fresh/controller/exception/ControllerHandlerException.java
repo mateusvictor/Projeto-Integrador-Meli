bb1a89ab08443840145453cfc214082d0d8407c1
gbarrettoml <gustavo.barretto@mercadolivre.com>
2022-04-25 12:29:46 -0300

    refactor: creating exception handler and exceptions

---
+++ src/main/java/br/com/meli/fresh/controller/exception/ControllerHandlerException.java
@@ -0,0 +1,4 @@
+package br.com.meli.fresh.controller.exception;
+
+public class ControllerHandlerException {
+}
---
+++ src/main/java/br/com/meli/fresh/model/exception/Exception.java
@@ -0,0 +1,4 @@
+package br.com.meli.fresh.model.exception;
+
+public class Exception {
+}
