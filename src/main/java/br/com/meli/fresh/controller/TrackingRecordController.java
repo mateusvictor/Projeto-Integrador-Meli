52806f92ee924b2d222ae2ad070f5ed95977653e
mateusvictor <mateus.ssilva@mercadolivre.com>
2022-05-04 17:38:04 -0300

    wip: add 'order-tracking' endpoints

---
+++ src/main/java/br/com/meli/fresh/controller/TrackingRecordController.java
@@ -0,0 +1,82 @@
+package br.com.meli.fresh.controller;
+
+import br.com.meli.fresh.assembler.TrackingRecordMapper;
+import br.com.meli.fresh.dto.request.TrackingRecordRequest;
+import br.com.meli.fresh.dto.response.TrackingRecordResponse;
+import br.com.meli.fresh.model.OrderStatus;
+import br.com.meli.fresh.model.PurchaseOrder;
+import br.com.meli.fresh.model.TrackingRecord;
+import br.com.meli.fresh.repository.IPurchaseOrderRepository;
+import br.com.meli.fresh.services.exception.EntityNotFoundException;
+import br.com.meli.fresh.services.impl.TrackingService;
+import lombok.AllArgsConstructor;
+import org.hibernate.criterion.Order;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(value = "api/v1/fresh-products/order-tracking")
+public class TrackingRecordController {
+    private final TrackingService service;
+    private final IPurchaseOrderRepository orderRepository;
+    private final TrackingRecordMapper mapper;
+
+    @GetMapping("/history/{orderId}")
+    public ResponseEntity<List<TrackingRecordResponse>> getOrderTrackHistory(@PathVariable String orderId){
+        return ResponseEntity.ok(
+                mapper.toResponseObject(service.getTrackingRecordList(orderId))
+        );
+    }
+
+    @PostMapping()
+    public ResponseEntity<TrackingRecordResponse> createTrackingRecord(@RequestBody TrackingRecordRequest recordDTO,
+                                                                       UriComponentsBuilder uriBuilder){
+        TrackingRecord trackingRecord = service.create(this.requestDTOToEntity(recordDTO));
+
+        URI uri = uriBuilder
+                .path("/{id}")
+                .buildAndExpand(trackingRecord.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(
+                mapper.toResponseObject(trackingRecord)
+        );
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TrackingRecordResponse> updateTrackingRecord(@PathVariable String id,
+                                                                       @RequestBody TrackingRecordRequest recordDTO,
+                                                                       UriComponentsBuilder uriBuilder){
+        TrackingRecord trackingRecord = service.update(id, this.requestDTOToEntity(recordDTO));
+
+        URI uri = uriBuilder
+                .path("/{id}")
+                .buildAndExpand(trackingRecord.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(
+                mapper.toResponseObject(trackingRecord)
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TrackingRecordResponse> getTrackingRecord(@PathVariable String id){
+        TrackingRecord trackingRecord = service.getById(id);
+        return ResponseEntity.ok(
+                mapper.toResponseObject(trackingRecord)
+        );
+    }
+
+    private TrackingRecord requestDTOToEntity(TrackingRecordRequest recordDTO){
+        TrackingRecord trackingRecord = mapper.toDomainObject(recordDTO);
+        PurchaseOrder order = orderRepository.findById(recordDTO.getOrderId())
+                .orElseThrow(() -> new EntityNotFoundException("Invalid orderId: " + recordDTO.getOrderId()));
+        trackingRecord.setPurchaseOrder(order);
+        return trackingRecord;
+    }
+}
